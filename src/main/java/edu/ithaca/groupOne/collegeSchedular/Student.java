228dda196fb5121202d0d9d6acef98eef63f208d
cweil00 <cweil@ithaca.edu>
2022-03-31 15:22:39 -0400

    made method headers

--- src/main/java/edu/ithaca/groupOne/collegeSchedular/Student.java
+++ src/main/java/edu/ithaca/groupOne/collegeSchedular/Student.java
@@ -2,4 +2,31 @@ package edu.ithaca.groupOne.collegeSchedular;
 
 public class Student {
     
+    public Student(){
+
+    }
+
+    public void createSchedule(){
+
+    }
+
+    public void addCourseToSchedule(){
+
+    }
+
+    public void removeSchedule(){
+
+    }
+
+    public void changeMajor(){
+
+    }
+
+    public String getMajor(){
+        return "";
+    }
+
+    public Schedule[] getSchedules(){
+        return null;
+    }
 }
