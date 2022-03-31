c17058e89dea9524515d4839afef1d58043a0d1e
cweil00 <cweil@ithaca.edu>
2022-03-31 15:37:24 -0400

    made testing headers

--- src/test/java/edu/ithaca/groupOne/collegeSchedular/StudentTest.java
+++ src/test/java/edu/ithaca/groupOne/collegeSchedular/StudentTest.java
@@ -1,5 +1,33 @@
 package edu.ithaca.groupOne.collegeSchedular;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class StudentTest {
     
+    @Test
+    void createScheduleTest(){
+        
+    }
+
+    @Test
+    void addCourseToScheduleTest(){
+
+    }
+
+    @Test
+    void removeScheduleTest(){
+
+    }
+
+    @Test
+    void changeMajorTest(){
+
+    }
+
+    @Test
+    void getterTests(){
+        
+    }
 }
