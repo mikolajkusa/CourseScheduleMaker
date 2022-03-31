95e700eebaa1de1f62e7cdf107055b2b73bd2960
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2022-03-31 15:26:03 -0400

    Created Person and PersonTest classes

---
+++ src/main/java/edu/ithaca/groupOne/collegeSchedular/Person.java
@@ -0,0 +1,17 @@
+package edu.ithaca.groupOne.collegeSchedular;
+
+public abstract class Person {
+    int id;
+    String password;
+
+    public boolean login(int enteredID, String enteredPassword){
+        if(enteredID != id){
+            return false;
+        }
+        else if(enteredPassword.compareTo(password) == 0){
+            return false;
+        }
+
+        return true;
+    }
+}
