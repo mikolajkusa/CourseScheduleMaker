79a587566611dc3fb95042892d45b879bbc0257a
Will Regalado Succop <wregaladosuccop@ithaca.edu>
2022-04-04 11:15:50 -0400

    not sure tbh

--- src/main/java/edu/ithaca/groupOne/collegeSchedular/Institution.java
+++ src/main/java/edu/ithaca/groupOne/collegeSchedular/Institution.java
@@ -2,7 +2,6 @@ package edu.ithaca.groupOne.collegeSchedular;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 
 public class Institution {
--- src/test/java/edu/ithaca/groupOne/collegeSchedular/InstitutionTest.java
+++ src/test/java/edu/ithaca/groupOne/collegeSchedular/InstitutionTest.java
@@ -6,7 +6,7 @@ public class InstitutionTest {
     
     @Test
     public void addGetPersonTest(){
-
+        
     }
 
     @Test
