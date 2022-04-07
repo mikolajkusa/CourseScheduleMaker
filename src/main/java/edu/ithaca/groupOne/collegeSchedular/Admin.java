ffdd9c185b6f3f7f30ba0cd59b160bdd99bba831
mikolajkusa <mkonieczny@ithaca.edu>
2022-04-07 14:39:09 -0400

    Added headers to admin class

--- src/main/java/edu/ithaca/groupOne/collegeSchedular/Admin.java
+++ src/main/java/edu/ithaca/groupOne/collegeSchedular/Admin.java
@@ -1,5 +1,30 @@
 package edu.ithaca.groupOne.collegeSchedular;
 
 public class Admin extends Person{
+
+    
+    
+    public Admin(String id, String password){
+        this.id = id;
+        this.password = password;
+    }
+
+    /**
+     * Removes a student from a course.
+     * @param student
+     * @param course
+     */
+    public void removeStudent(Student student, Course course){
+        
+    }
+
+    /**
+     * Adds a new student to a course
+     * @param student
+     * @param course
+     */
+    public void addStudent(Student student, Course course){
+
+    }
     
 }
