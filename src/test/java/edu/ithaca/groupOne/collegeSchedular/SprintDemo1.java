5bf63a7d32a64136579e4aae5c800991058914a4
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2022-04-07 13:16:25 -0400

    Added comments to plan out sprint demo

--- src/test/java/edu/ithaca/groupOne/collegeSchedular/SprintDemo1.java
+++ src/test/java/edu/ithaca/groupOne/collegeSchedular/SprintDemo1.java
@@ -2,4 +2,17 @@ package edu.ithaca.groupOne.collegeSchedular;
 
 public class SprintDemo1 {
     
+    //Login demo
+
+    //Institution class demo
+
+    //Show class diagram
+
+    //Show sequence diagram
+
+    //Change major
+
+    //Course library demo
+
+    //Student and Abstract Person demo
 }
