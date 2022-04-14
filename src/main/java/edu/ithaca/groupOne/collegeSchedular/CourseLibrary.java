2e1460b0b44926411e89e4008ba34e05a9aab9ff
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2022-04-14 15:28:37 -0400

    Created files for CourseLibrary and Test

---
+++ src/main/java/edu/ithaca/groupOne/collegeSchedular/CourseLibrary.java
@@ -0,0 +1,23 @@
+package edu.ithaca.groupOne.collegeSchedular;
+
+import java.util.ArrayList;
+
+public class CourseLibrary {
+    private ArrayList<Course> courses;
+
+    public CourseLibrary(){
+        courses = new ArrayList<Course>();
+    }
+
+    public ArrayList<Course> getCourses(){
+        return null;
+    }
+
+    public void addCourse(){
+
+    }
+
+    public void removeCourse(){
+        
+    }
+}
---
+++ src/test/java/edu/ithaca/groupOne/collegeSchedular/CourseLibraryTest.java
@@ -0,0 +1,21 @@
+package edu.ithaca.groupOne.collegeSchedular;
+
+import org.junit.jupiter.api.Test;
+
+public class CourseLibraryTest {
+    
+    @Test
+    void constructorTest(){
+        
+    }
+
+    @Test
+    void addCourseTest(){
+
+    }
+
+    @Test
+    void removeCourseTest(){
+
+    }
+}
