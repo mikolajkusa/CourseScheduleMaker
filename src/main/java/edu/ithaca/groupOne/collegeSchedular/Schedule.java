8f55617d3e90ed309536c1650d0f49a38f036f2a
mikolajkusa <mkonieczny@ithaca.edu>
2022-04-14 15:21:30 -0400

    Added headers for schedule class

--- src/main/java/edu/ithaca/groupOne/collegeSchedular/Schedule.java
+++ src/main/java/edu/ithaca/groupOne/collegeSchedular/Schedule.java
@@ -1,5 +1,40 @@
 package edu.ithaca.groupOne.collegeSchedular;
 
+import java.util.ArrayList;
+
 public class Schedule {
+    private String name;
+    private ArrayList<Course> courses;
+
+    public Schedule(String name){
+        this.name = name;
+        courses = new ArrayList<Course>();
+    }
+    /**
+     * Adds a course to the schedule
+     * @throws TimeConflictException if the new course conflicts with pre-existing course
+     */
+    public void addCourse(){}
+    
+    /**Removes a course from the schedule
+     * @throws CourseNotFoundException if the course is not in the schedule
+     */
+    public void removeCourse(){}
+    
+    /**
+     * Returns the name assigned to the schedule
+     * @return
+     */
+    public String getName(){
+        return name;
+    }
+    
+    /**
+     * Returns a list of courses in the schedule
+     * @return
+     */
+    public ArrayList<Course> getCourses(){
+        return courses;
+    }
 
 }
