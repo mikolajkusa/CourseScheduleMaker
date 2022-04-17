df363e1c4457707d0afb7a47b970b0db32001a13
mikolajkusa <mkonieczny@ithaca.edu>
2022-04-17 16:03:51 -0400

    Implemented tests for schedule

--- src/main/java/edu/ithaca/groupOne/collegeSchedular/Schedule.java
+++ src/main/java/edu/ithaca/groupOne/collegeSchedular/Schedule.java
@@ -14,12 +14,12 @@ public class Schedule {
      * Adds a course to the schedule
      * @throws TimeConflictException if the new course conflicts with pre-existing course
      */
-    public void addCourse(){}
+    public void addCourse(Course course){}
     
     /**Removes a course from the schedule
      * @throws CourseNotFoundException if the course is not in the schedule
      */
-    public void removeCourse(){}
+    public void removeCourse(Course course){}
     
     /**
      * Returns the name assigned to the schedule
