fea74a655a7865b75c804b13e2682d69899378b8
mikolajkusa <mkonieczny@ithaca.edu>
2022-04-17 16:31:48 -0400

    Implemented schedule, passes all tests

--- src/main/java/edu/ithaca/groupOne/collegeSchedular/Schedule.java
+++ src/main/java/edu/ithaca/groupOne/collegeSchedular/Schedule.java
@@ -14,12 +14,33 @@ public class Schedule {
      * Adds a course to the schedule
      * @throws TimeConflictException if the new course conflicts with pre-existing course
      */
-    public void addCourse(Course course){}
+    public void addCourse(Course course) throws TimeConflictException{
+        String timeSlot = course.getTimeSlot();
+        String[] courseSlot = timeSlot.split(" ");
+        for(int i = 0; i<courses.size();i++){
+            String[] oldCourseTime = courses.get(i).getTimeSlot().split(" ");
+            if(courseSlot[0].equals(oldCourseTime[0])){
+                String[] newTime = courseSlot[1].split("-");
+                String[] oldTime = oldCourseTime[1].split("-");
+                if(Double.parseDouble(newTime[0])<Double.parseDouble(oldTime[1]) && Double.parseDouble(newTime[1])>Double.parseDouble(oldTime[0])){
+                    throw new TimeConflictException("The time slots overlap; can't add new course to schedule");
+                }
+
+            }
+            
+        }
+        courses.add(course);
+    }
     
     /**Removes a course from the schedule
      * @throws CourseNotFoundException if the course is not in the schedule
      */
-    public void removeCourse(Course course){}
+    public void removeCourse(Course course) throws CourseNotFoundException{
+        if(!courses.contains(course)){
+            throw new CourseNotFoundException("This course is not in the schedule");
+        }
+        courses.remove(course);
+    }
     
     /**
      * Returns the name assigned to the schedule
--- src/test/java/edu/ithaca/groupOne/collegeSchedular/ScheduleTest.java
+++ src/test/java/edu/ithaca/groupOne/collegeSchedular/ScheduleTest.java
@@ -8,12 +8,12 @@ import org.junit.jupiter.api.Test;
 
 public class ScheduleTest {
     @Test
-    public void addRemoveScheduleTest(){
+    public void addRemoveScheduleTest() throws TimeConflictException, CourseNotFoundException{
         Course phys2 = new Course(10200, 20, 4, "Physics", "Spring", "MWF 8-10");
         Course calc3 = new Course(21100, 20, 4, "Math", "Spring", "MWF 9-10");
         Course calc4 = new Course(21200, 20, 4, "Math", "Spring", "TR 8-10");
         Course calc5 = new Course(21300, 20, 4, "Math", "Spring", "MWF 10-12");
-        Course calc6 = new Course(21300, 20, 4, "Math", "Spring", "TR 8-9");
+        Course calc6 = new Course(21300, 20, 4, "Math", "Spring", "MWF 8-9");
         Schedule mySchedule = new Schedule("Mikolaj's Schedule");
         
         //add-remove course in empty schedule (border case) 
