16b5bbcd31f58018329a44e5e6513d84c7254074
cweil00 <cweil@ithaca.edu>
2022-04-19 18:58:00 -0400

    made changeSEmesterTest

--- src/test/java/edu/ithaca/groupOne/collegeSchedular/ProfessorTest.java
+++ src/test/java/edu/ithaca/groupOne/collegeSchedular/ProfessorTest.java
@@ -52,6 +52,31 @@ public class ProfessorTest {
         assertThrows(IllegalArgumentException.class, ()->prof.changeCreditAmount(1, 0)); //boundary case - cannot have a course with zero credits
         assertEquals(3, prof.getCourses().get(1).getCredits());
         assertThrows(IllegalArgumentException.class, ()->prof.changeCreditAmount(2, 4)); //equivalence case - invalid course id
+        prof.createCourse(2, 10, 4, "computer science", "sf2022", "MWF10-1050"); //makes sure semester is tested during course creation
+        changeSemesterTest(); //more tests for making sure new semster is valid
+    }
+
+    @Test
+    void changeSemesterTest() throws CourseIdInUseException, IllegalArgumentException{
+        Professor prof = new Professor("2002", "password");
+        prof.createCourse(1, 10, 4, "computer science", "sp2022", "MWF10-1050");
+        assertEquals("sp2022", prof.getCourses().get(1).getSemester()); //equivalance case - no change in semester yet
+        assertThrows(IllegalArgumentException.class, ()-> prof.changeCourseSemester(2, "sp2022")); //equivalence case - invalid course id
+        prof.changeCourseSemester(1, "fa2022");
+        assertEquals("fa2022", prof.getCourses().get(1).getSemester()); //equivalence case - valid new semester
+        assertThrows(IllegalArgumentException.class, ()->prof.changeCourseSemester(1, "")); //equivalence case - invalid new semester
+        assertThrows(IllegalArgumentException.class, ()->prof.changeCourseSemester(1, "bfhdaf"));
+        assertThrows(IllegalArgumentException.class, ()->prof.changeCourseSemester(1, "fp2021"));
+        prof.changeCourseSemester(1, "wi2022"); //equivalence case - winter semester
+        assertEquals("wi2022", prof.getCourses().get(1).getSemester());
+        prof.changeCourseSemester(1, "su2022"); //equivalence case - summer semester
+        assertEquals("su2022", prof.getCourses().get(1).getSemester());
+        prof.changeCourseSemester(1, "Su2022");
+        assertEquals("fa2022", prof.getCourses().get(1).getSemester()); //equivalence case - ignore case
+        assertThrows(IllegalArgumentException.class, ()->prof.changeCourseSemester(1, "fa20a10")); //equivalence class - year is invalid
+        assertThrows(IllegalArgumentException.class, ()->prof.changeCourseSemester(1, "fa2021.9")); //equivalence class - year is a floating point number
+        assertThrows(IllegalArgumentException.class, ()->prof.changeCourseSemester(1, "fa-2021")); //equivalance case - year is negative
+        //do we want to allow any possible year between 0 and infinity?
     }
     
 }
