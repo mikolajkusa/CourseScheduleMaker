d33a9084be3da982dc7cee01cb8172f6fd6a23be
Will Regalado Succop <wregaladosuccop@ithaca.edu>
2022-04-27 10:23:44 -0400

    another test

--- src/test/java/edu/ithaca/groupOne/collegeSchedular/displayScheduleTest.java
+++ src/test/java/edu/ithaca/groupOne/collegeSchedular/displayScheduleTest.java
@@ -2,9 +2,8 @@ package edu.ithaca.groupOne.collegeSchedular;
 
 public class displayScheduleTest {
     
-    public static void main(String[] args) throws TimeConflictException {
-
-        Course phys2 = new Course(10200, 20, 4, "Physics", "Spring", "MWF 9-10", "Physics");
+    private static void displayScheduleOne() throws TimeConflictException {
+        Course phys2 = new Course(10200, 20, 4, "Math", "Spring", "MWF 9-10", "Physics");
         Course calc3 = new Course(21100, 20, 4, "Math", "Spring", "MWF 10-11", "Calc1");
         Course calc4 = new Course(21200, 20, 4, "Math", "Spring", "TR 9-10", "English");
         Course calc5 = new Course(21300, 20, 4, "Math", "Spring", "MWF 11-12", "History");
@@ -16,6 +15,27 @@ public class displayScheduleTest {
         mySchedule.addCourse(calc5);
         mySchedule.addCourse(calc6);
         displaySchedule.genSchedule(mySchedule);
+    }
+
+    private static void displayScheduleTwo() throws TimeConflictException {
+        Course phys2 = new Course(10200, 20, 4, "Math", "Spring", "TR 2-3", "Art");
+        Course calc3 = new Course(21100, 20, 4, "Math", "Spring", "MWF 1-2", "Comp Sci");
+        Course calc4 = new Course(21200, 20, 4, "Math", "Spring", "TR 9-10", "Philo");
+        Course calc5 = new Course(21300, 20, 4, "Math", "Spring", "MWF 11-12", "Design");
+        Course calc6 = new Course(21300, 20, 4, "Math", "Spring", "TR 3-4", "French");
+        Schedule mySchedule = new Schedule("Mikolaj's Schedule");
+        mySchedule.addCourse(phys2);
+        mySchedule.addCourse(calc3);
+        mySchedule.addCourse(calc4);
+        mySchedule.addCourse(calc5);
+        mySchedule.addCourse(calc6);
+        displaySchedule.genSchedule(mySchedule);
+    }
+
+    public static void main(String[] args) throws TimeConflictException {
+        //displayScheduleOne();
+        displayScheduleTwo();
+        
     }
 
 }
