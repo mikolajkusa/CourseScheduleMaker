2484b75c95f16e95d3588030626ed63fb25190a3
cweil00 <cweil@ithaca.edu>
2022-05-04 15:49:29 -0400

    fixed course names in demo

--- src/test/java/edu/ithaca/groupOne/collegeSchedular/ScheduleMaker.java
+++ src/test/java/edu/ithaca/groupOne/collegeSchedular/ScheduleMaker.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class ScheduleMaker {
     public static void main(String[] args){
         CourseLibrary lib = new CourseLibrary();
-        lib.addCourse(new Course(34500, 30, 4, "Computer Science", "Spring", "MWF 11-12","pp"));
-        lib.addCourse(new Course(10200, 30, 4, "Physics", "Spring", "MWF 8-10","woosh"));
-        lib.addCourse(new Course(11800, 30, 4, "Physics", "Spring", "MWF 10-12","boi"));
-        lib.addCourse(new Course(21800, 30, 4, "Physics", "Spring", "MWF 10-11","super"));
-        lib.addCourse(new Course(17500, 30, 4, "Writing", "Spring", "TR 11-12","what"));
-        lib.addCourse(new Course(21100, 30, 4, "Maths", "Spring", "MWF 9-10","conductrz"));
-        lib.addCourse(new Course(49000, 30, 4, "Philosophy", "Spring", "MWF 3-4","logic"));
-        lib.addCourse(new Course(10500, 30, 6, "Gender Studies", "Spring", "TR 1-3","chris"));
-        lib.addCourse(new Course(31100, 30, 4, "Psychology", "Spring", "TR 8-9","womenpsych"));
-        lib.addCourse(new Course(21700, 30, 4, "Botany", "Spring", "MWF 7-9","comp3"));
+        lib.addCourse(new Course(34500, 30, 4, "Computer Science", "Spring", "MWF 11-12","COMP345"));
+        lib.addCourse(new Course(10200, 30, 4, "Physics", "Spring", "MWF 8-10","PHYS102"));
+        lib.addCourse(new Course(11800, 30, 4, "Physics", "Spring", "MWF 10-12","PHYS118"));
+        lib.addCourse(new Course(21800, 30, 4, "Physics", "Spring", "MWF 10-11","PHYS218"));
+        lib.addCourse(new Course(17500, 30, 4, "Writing", "Spring", "TR 11-12","WRTG175"));
+        lib.addCourse(new Course(21100, 30, 4, "Maths", "Spring", "MWF 9-10","MATH211"));
+        lib.addCourse(new Course(49000, 30, 4, "Philosophy", "Spring", "MWF 3-4","PHIL490"));
+        lib.addCourse(new Course(10500, 30, 6, "Gender Studies", "Spring", "TR 1-3","GS105"));
+        lib.addCourse(new Course(31100, 30, 4, "Psychology", "Spring", "TR 8-9","PSYC311"));
+        lib.addCourse(new Course(21700, 30, 4, "Botany", "Spring", "MWF 7-9","BOT217"));
 
 
         Schedule mySchedule = new Schedule("My Schedule");
